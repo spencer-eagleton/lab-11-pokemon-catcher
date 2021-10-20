29539d8b2825635445573663de86ee2ce5f44e98
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-20 13:48:28 -0700

    created both html files and linked them together. placeholder images

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-// import functions and grab DOM elements
+// import { pokemon } from './pokemon.js';
+// console.log(pokemon);
 
-// initialize global state
+// const generatePokemon = () =>{
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+//   // generate 3 random pokemon
+//     let randNum1 = Math.floor(Math.random() * pokemon.length);
+//     let randNum2 = Math.floor(Math.random() * pokemon.length);
+//     let randNum3 = Math.floor(Math.random() * pokemon.length);
+
+
+//   // regenerate number if they match
+//     while (randNum1 === randNum2 ||
+//       randNum1 === randNum3 ||
+//       randNum2 === randNum3
+//     ) { 
+//         randNum1 = Math.floor(Math.random() * pokemon.length);
+//         randNum2 = Math.floor(Math.random() * pokemon.length);
+//         randNum3 = Math.floor(Math.random() * pokemon.length);
+//     }
+//      let pokemon1 = pokemon[randNum1];
+//      pokemon1Img.src = pokemon1.image;
+
+//      let pokemon2 = pokemon[randNum2];
+//      pokemon2Img.src = pokemon2.image;
+
+//      let pokemon3 = pokemon[randNum3];
+//      pokemon3Img.src = pokemon3.image;
+
+//
+//
+//   // call showPokemon for each
+//   // render pokemon to page
+
+
+// };
+// generatePokemon();
